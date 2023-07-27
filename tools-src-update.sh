6fc147574c1431d3ebbc779feaa7322810229756
Romain7426 <romain7426@example.com>
2023-07-27 17:52:43 +0000

    e

--- tools-src-update.sh
+++ tools-src-update.sh
@@ -7,7 +7,9 @@ mkdir -p tools/src
 mkdir -p tools/build
 mkdir -p tools/bin
 
-cp -fp ../project-tools/src/*.c tools/src/
+cp -fp ../project-tools/src/*.c  tools/src/
+cp -fp ../project-tools/src/*.ci tools/src/
 
 chmod -w tools/src/*.c
+chmod -w tools/src/*.ci
 
