cb28f5065b1555891393654adfab319a4dc60cae
Romain7426 <romain7426@example.com>
2025-03-04 08:30:09 +0000

    e

---
+++ src/006_check_and_assert.c
@@ -0,0 +1,21 @@
+#include "global.h"
+#include "006_check_and_assert.h"
+#include "texture.h"
+#include "camera.h"
+
+
+
+
+void check_and_assert(const int8_t debug_print_huh, const int stderr_d) { 
+  int_buffered_outstream__error__check_and_assert(); 
+  CTexture__check_and_assert(debug_print_huh, stderr_d); 
+  CCamera__check_and_assert(debug_print_huh, stderr_d); 
+  CMoteurTeleportation__check_and_assert(debug_print_huh, stderr_d); 
+  CZoneTeleportation__check_and_assert(debug_print_huh, stderr_d); 
+  CMap__check_and_assert(debug_print_huh, stderr_d); 
+  CSol__check_and_assert(debug_print_huh, stderr_d); 
+  CAffichageCoeur__check_and_assert(debug_print_huh, stderr_d); 
+  CAffichageMainPierre__check_and_assert(debug_print_huh, stderr_d); 
+  CPageTitre__check_and_assert(debug_print_huh, stderr_d); 
+}; 
+
---
+++ src/006_check_and_assert.h
@@ -0,0 +1,6 @@
+#ifndef CHECK_AND_ASSERT_H
+#define CHECK_AND_ASSERT_H
+
+extern void check_and_assert(const int8_t debug_print_huh, const int stderr_d); 
+
+#endif /* CHECK_AND_ASSERT_H */
